579a0e6285dc3d89950b2bdecf47143c473eff7a
michael-doll <MWDoll88@gmail.com>
2019-10-18 09:12:45 -0400

    Completed table tests

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,23 +2,43 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        StringBuilder tableNums = new StringBuilder();
+      StringBuilder tableNums = new StringBuilder();
+        
         int size = 5; 
         for(int i = 1; i <= size; i++){
             for(int j = 1; j<=size; j++){
-                tableNums.append("  " +j*i + " |"); 
+                tableNums.append(String.format("%3d",j*i)).append(" |"); 
+               
             }
             tableNums.append("\n"); 
             }
     String table = tableNums.toString();
     return table;
+
 }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder tableNums = new StringBuilder();
+        int size = 10; 
+        for(int i = 1; i <= size; i++){
+            for(int j = 1; j<=size; j++){
+                tableNums.append(String.format("%3d",j*i)).append(" |"); 
+            }
+            tableNums.append("\n"); 
+            }
+    String table = tableNums.toString();
+    return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder tableNums = new StringBuilder();
+        for(int i = 1; i <= tableSize; i++){
+            for(int j = 1; j<=tableSize; j++){
+                tableNums.append(String.format("%3d",j*i)).append(" |"); 
+            }
+            tableNums.append("\n"); 
+            }
+    String table = tableNums.toString();
+    return table;
     }
 }
